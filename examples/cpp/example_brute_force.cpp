b455bd19c50b5e96adc31d53feb9f2e65bb0c67e
Andrew Witten <awitten1@terpmail.umd.edu>
2026-09-25 09:40:47 -0400

    add hnsw to example

--- examples/cpp/example_brute_force.cpp
+++ examples/cpp/example_brute_force.cpp
@@ -1,51 +1,101 @@
 
 
 #include "../../hnswlib/hnswlib.h"
+#include <chrono>
 #include <cstdio>
 #include <cstdlib>
 #include <mutex>
 #include <iostream>
+#include <numeric>
 #include <random>
+#include <ratio>
+#include <string>
+#include <vector>
 
-float* get_random_vector(int dim) {
-  static std::mt19937 rng;
-  static std::once_flag of;
-  std::call_once(of, [&]() {
-      rng.seed(47);
-  });
+std::vector<float> get_random_vector(int dim) {
+  static std::mt19937 rng(42);
   std::uniform_real_distribution<> distrib_real;
-  float* data = new float[dim];
+  std::vector<float> data(dim);
   for (int i = 0; i < dim; i++) {
       data[i] = distrib_real(rng);
   }
   return data;
 }
 
-int main(int argc, char** argv) {
+struct Parameters {
   int dim = 32;
+  int num_elements = 0;
+  bool brute_force = true;
+};
+
+Parameters parse_args(int argc, char** argv, int max_elements) {
+  Parameters params;
+  for (int i = 1; i < argc; i += 2) {
+    std::string option = argv[i];
+    if (option == "--brute_force") {
+      std::string value = i + 1 < argc ? argv[i + 1] : "";
+      if (value != "true" && value != "false") {
+        fprintf(stderr, "--brute_force must be true or false\n");
+        exit(EXIT_FAILURE);
+      }
+      params.brute_force = value == "true";
+      continue;
+    }
+    char* end = nullptr;
+    long value = i + 1 < argc ? std::strtol(argv[i + 1], &end, 10) : 0;
+    if (value <= 0 || value > max_elements || *end ||
+        (option != "--dim" && option != "--num_elements")) {
+      fprintf(stderr, "Usage: %s --num_elements N [--dim D] [--brute_force true|false]\n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
+    (option == "--dim" ? params.dim : params.num_elements) = static_cast<int>(value);
+  }
+  if (params.num_elements < params.dim) {
+    fprintf(stderr, "--num_elements is required and must be at least --dim\n");
+    exit(EXIT_FAILURE);
+  }
+  return params;
+}
+
+int main(int argc, char** argv) {
   int max_elements = 1e7;
-  hnswlib::L2Space l2_space(dim);
-  hnswlib::AlgorithmInterface<float>* bf = new hnswlib::BruteforceSearch<float>(&l2_space, max_elements);
-
-  if (argc < 2) {
-    fprintf(stderr, "must pass in num_elements\n");
-    exit(EXIT_FAILURE);
+  const Parameters params = parse_args(argc, argv, max_elements);
+  hnswlib::L2Space l2_space(params.dim);
+  hnswlib::AlgorithmInterface<float>* index;
+  if (params.brute_force) {
+    index = new hnswlib::BruteforceSearch<float>(&l2_space, max_elements);
   }
-  int num_elements = std::stoi(argv[1]);
-  if (num_elements > max_elements) {
-    fprintf(stderr, "num_elements must be less than %d", max_elements);
-    exit(EXIT_FAILURE);
+  else {
+    index = new hnswlib::HierarchicalNSW<float>(&l2_space, max_elements);
   }
 
-  for (int i = 0; i < num_elements; ++i) {
-    bf->addPoint(get_random_vector(dim), i);
+  for (int i = 0; i < params.num_elements; ++i) {
+    auto data = get_random_vector(params.dim);
+    index->addPoint(data.data(), i);
   }
 
-  for (int i = 0; i < 1000; ++i) {
-    bf->searchKnn(get_random_vector(dim), dim);
+  int trials = 1000;
+  float* trial_durations_ms = new float[trials];
+  for (int i = 0; i < trials; ++i) {
+    auto t1 = std::chrono::system_clock::now();
+    auto data = get_random_vector(params.dim);
+    index->searchKnn(data.data(), params.dim);
+    std::chrono::duration<float, std::milli> d = std::chrono::system_clock::now() - t1;
+
+    trial_durations_ms[i] = d.count();
   }
+
+  std::sort(trial_durations_ms, trial_durations_ms + trials);
+  float total_duration_ms = std::accumulate(trial_durations_ms, trial_durations_ms + trials, 0);
+
+  std::cout << "avg_ms=" << total_duration_ms/trials
+    << ",p25=" << trial_durations_ms[trials/4]
+    << ",p50=" << trial_durations_ms[trials/2]
+    << ",p75=" << trial_durations_ms[3*trials/4] << std::endl;
+
+  delete[] trial_durations_ms;
+  delete index;
 
   return 0;
 
 }
-
